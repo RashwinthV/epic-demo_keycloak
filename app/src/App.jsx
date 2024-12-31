b1698ceb897c3778bf273d29a967335024599676
tharun <vrashwinth@gmail.com>
2024-12-31 16:43:28 +0530

    new commit

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Body from "./components/Body";
+import Body from "./components/Body1";
 import Header from "./components/Header";
 import UseAuth from "./Hooks/UseAuth";
 import Unauthorized from "./components/Unauthorized";
--- app/src/components/Unauthorized.jsx
+++ app/src/components/Unauthorized.jsx
@@ -2,7 +2,7 @@ import '../styles/Error.css';
 
 function Unauthorized() {
   return (
-    <div>
+    <div className='back'>
 <div className="container">  
 <h1 className="warning">⚠️ Unauthorized Access</h1>
 <p>You do not have permission or you have not initialized the <b style={{color:'black'}}> keycloak server</b> to view this page.</p>
